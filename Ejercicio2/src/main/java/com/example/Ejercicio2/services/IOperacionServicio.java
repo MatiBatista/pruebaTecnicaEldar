a596f2b1f6f5c7172f51da5ed45a06e7b9f1f89a
MatiBatista <matiasomarbatista1@gmail.com>
2023-11-10 15:56:19 -0300

    Se especifico e implemento la operacion para obtener la tasa de una operacion

--- Ejercicio2/src/main/java/com/example/Ejercicio2/services/IOperacionServicio.java
+++ Ejercicio2/src/main/java/com/example/Ejercicio2/services/IOperacionServicio.java
@@ -1,8 +1,12 @@
 package com.example.Ejercicio2.services;
 
+import com.example.Ejercicio2.dtos.request.MarcaEImporteRequest;
 import com.example.Ejercicio2.dtos.response.OperacionResponse;
+import com.example.Ejercicio2.dtos.response.TasaOperacionResponse;
 
 
 public interface IOperacionServicio {
     OperacionResponse getInformacionOperacion(long idOperacion);
+
+    TasaOperacionResponse getTasaDeOperacion(MarcaEImporteRequest marcaEImporteRequest);
 }
--- Ejercicio2/src/main/java/com/example/Ejercicio2/services/impl/OperacionServicio.java
+++ Ejercicio2/src/main/java/com/example/Ejercicio2/services/impl/OperacionServicio.java
@@ -1,13 +1,20 @@
 package com.example.Ejercicio2.services.impl;
 
+import com.example.Ejercicio2.dtos.request.MarcaEImporteRequest;
 import com.example.Ejercicio2.dtos.response.OperacionResponse;
+import com.example.Ejercicio2.dtos.response.TasaOperacionResponse;
+import com.example.Ejercicio2.exceptions.ConsumoExcedidoException;
 import com.example.Ejercicio2.exceptions.ResourceNotFoundException;
 import com.example.Ejercicio2.models.Marca;
 import com.example.Ejercicio2.models.Operacion;
+import com.example.Ejercicio2.models.estrategiasMarcas.EstrategiaTasaAmex;
+import com.example.Ejercicio2.models.estrategiasMarcas.EstrategiaTasaNara;
+import com.example.Ejercicio2.models.estrategiasMarcas.EstrategiaTasaVisa;
 import com.example.Ejercicio2.repositories.OperacionRepository;
 import com.example.Ejercicio2.services.IOperacionServicio;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
 
 
 @Service
@@ -36,4 +43,34 @@ public class OperacionServicio implements IOperacionServicio {
 
         return OperacionResponse.builder().nombreMarcaTarjeta(nombreMarca).importeInicial(importeInicial).importeFinal(montoFinal).tasaServicio(tasaMarca).build();
     }
+
+    @Override
+    public TasaOperacionResponse getTasaDeOperacion(MarcaEImporteRequest marcaEImporteRequest) {
+        final float importeInicial = marcaEImporteRequest.getMonto();
+        final String nombreMarca = marcaEImporteRequest.getNombreMarca();
+        if (!Operacion.esValida(importeInicial)) {
+            throw new ConsumoExcedidoException("El monto de la operacion no puede superar los $1000");
+        }
+        Marca marca = crearMarcaSegunNombre(nombreMarca);
+
+        float tasaDeOperacion = calcularTasaDeOperacion(importeInicial, marca);
+
+        return TasaOperacionResponse.builder().tasaDeOperacion(tasaDeOperacion).build();
+    }
+
+    private Marca crearMarcaSegunNombre(String nombreMarca) {
+        return switch (nombreMarca) {
+            case "AMEX" -> new Marca("AMEX", new EstrategiaTasaAmex());
+            case "VISA" -> new Marca("VISA", new EstrategiaTasaVisa());
+            case "NARA" -> new Marca("NARA", new EstrategiaTasaNara());
+            default -> throw new ResourceNotFoundException("Marca de tarjeta '" + nombreMarca + "' no encontrada");
+        };
+    }
+
+    private float calcularTasaDeOperacion(float importeInicial, Marca marca) {
+        float tasaMarca = marca.calcularTasa();
+        return tasaMarca / 100.0f * importeInicial;
+    }
+
+
 }
